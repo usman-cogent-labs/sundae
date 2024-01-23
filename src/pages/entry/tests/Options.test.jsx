d60c8af9c35f3cc8cac489cd8a6dba14886d9266
Usman Afzal <109211422+usman-cogent-labs@users.noreply.github.com>
2024-01-23 11:58:14 +0500

    MSW changes reverted

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Options from '../Options';
 
-test('displays image for each scope options from server', async () => {
+test('displays image for each scope options from server ', async () => {
   render(<Options optionType={'scoops'} />);
 
   const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
