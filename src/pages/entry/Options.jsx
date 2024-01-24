4499134bc09a4a568a51766e415013768b74c3ce
Usman Afzal <109211422+usman-cogent-labs@users.noreply.github.com>
2024-01-24 19:32:26 +0500

    Waited for two async calls

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -2,9 +2,11 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import ScoopOptions from './ScoopOptions';
 import ToppingOptions from './ToppingOptions';
+import AlertBanner from '../common/AlertBanner';
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   const ItemComponent = optionType === 'scoops' ? ScoopOptions : ToppingOptions;
 
@@ -20,8 +22,10 @@ export default function Options({ optionType }) {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((res) => setItems(res?.data))
-      .catch((err) => console.log(err));
-  }, []);
+      .catch((err) => setError(true));
+  }, [optionType]);
+
+  if (error) return <AlertBanner />;
 
   return <div>{optionItems}</div>;
 }
